34e7c90851920548ba79aa6bbe8922a9f5aa8c88
Ronihe <herongrong2011@gmail.com>
2019-03-05 08:55:59 -0800

    created the rootReducer for GET_NEW jokes

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,9 +1,12 @@
 import { VOTE_UP, VOTE_DOWN, GET_NEW } from './actionTypes';
 
-function rootReducer(state = [], action){
-  switch(action.type){
-    case VOTE_UP
-    
-    
+function rootReducer(state = [], action) {
+  switch (action.type) {
+    case GET_NEW:
+      return [...state, ...action.jokes];
+    default:
+      return state;
   }
 }
+
+export default rootReducer;
